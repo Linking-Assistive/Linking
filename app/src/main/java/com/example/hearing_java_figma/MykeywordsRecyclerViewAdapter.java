9f42e81c5d67398fb6f05fe0d02d3540079f5f2c
Lethehry <1318180434@qq.com>
2023-04-02 05:04:15 +0800

    item keyword update

--- app/src/main/java/com/example/hearing_java_figma/MykeywordsRecyclerViewAdapter.java
+++ app/src/main/java/com/example/hearing_java_figma/MykeywordsRecyclerViewAdapter.java
@@ -1,9 +1,15 @@
 package com.example.hearing_java_figma;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
 import com.example.hearing_java_figma.VO.KeywordTuple;
@@ -54,9 +60,62 @@ public class MykeywordsRecyclerViewAdapter extends RecyclerView.Adapter<Mykeywor
         public ViewHolder(FragmentKeywordsBinding binding) {
             super(binding.getRoot());
             /*mIdView = binding.itemNumber;*/
+            binding.content.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    KeywordsMenu(view);
+                }
+            });
+            binding.deleteButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+                    builder.setMessage("Are you sure you want to delete it?");
+                    builder.setCancelable(true);
+                    builder.setPositiveButton("Confirm",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    //put your code that needed to be executed when okay is clicked
+                                    AlertDialog.Builder builderok = new AlertDialog.Builder(view.getContext());
+                                    builderok.setMessage("Delete function not yet incomplete");
+                                    builderok.create().show();
+                                }
+                            });
+                    builder.setNegativeButton("Cancel",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    dialog.cancel();
+                                }
+                            });
+                    builder.create().show();
+                }
+            });
             mContentView = binding.content;
         }
+        private void KeywordsMenu(View view){
 
+            PopupMenu popupmenu = new PopupMenu(view.getContext(), view);
+            popupmenu.setGravity(Gravity.LEFT);
+            popupmenu.getMenuInflater().inflate(R.menu.keywords_menu, popupmenu.getMenu());
+
+            popupmenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem menuItem) {
+
+                    switch (menuItem.getItemId()){
+                        case R.id.activate_switch:
+
+                            break;
+                        case R.id.deactivate_keyword:
+
+                            break;
+                    }
+                    return true;
+                }
+            });
+            popupmenu.show();
+
+        }
         @Override
         public String toString() {
             return super.toString() + " '" + mContentView.getText() + "'";
